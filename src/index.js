b173eab6ad55bd420c58d4d46a4161cd6bb6c937
EllaCodes4 <105608215+EllaCodes4@users.noreply.github.com>
2022-07-01 14:30:22 -0500

    Added weather icon

--- src/index.js
+++ src/index.js
@@ -11,6 +11,12 @@ function displayWeather(response) {
   console.log(response.data);
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
+  let weatherIcon = document.querySelector("#weather-icon");
+  weatherIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  weatherIcon.setAttribute("alt", response.data.weather[0].description);
   let weatherDescription = document.querySelector("#weather-description");
   weatherDescription.innerHTML = response.data.weather[0].description;
   let temperatureElement = document.querySelector("#temperature");
