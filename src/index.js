862796d0170ee9cc9172cb95c7a3ce8b8597270c
EllaCodes4 <105608215+EllaCodes4@users.noreply.github.com>
2022-07-01 13:28:35 -0500

    Showing current weather using weather API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+function displayWeather(response) {
+  console.log(response.data);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let weatherDescription = document.querySelector("#weather-description");
+  weatherDescription.innerHTML = response.data.weather[0].description;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let highTemp = document.querySelector("#high-temp");
+  highTemp.innerHTML = Math.round(response.data.main.temp_max);
+  let lowTemp = document.querySelector("#low-temp");
+  lowTemp.innerHTML = Math.round(response.data.main.temp_min);
+}
 
+let apiKey = "39b37e744d3d61db56e033dc0b8a5694";
+let unit = "imperial";
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+
+axios.get(apiUrl).then(displayWeather);
